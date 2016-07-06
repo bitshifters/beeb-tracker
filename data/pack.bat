e1221b1dd576f5a3bb1d653279755daab883ed23
SimonM <podvilla@gmail.com>
2016-07-06 23:20:13 +0100

    Added volume2 - ninja gaiden compilation

--- data/pack.bat
+++ data/pack.bat
@@ -1,1 +1,3 @@
 exomizer.exe raw -c -m 1024 loader_volume1.bin -o loader_volume1.bin.exo
+exomizer.exe raw -c -m 1024 loader_volume2.bin -o loader_volume2.bin.exo
+exomizer.exe raw -c -m 1024 ninja_gaiden.bin -o ninja_gaiden.bin.exo
--- make.bat
+++ make.bat
@@ -1,3 +1,5 @@
 ..\..\Bin\beebasm.exe -i beebtrk.6502 -do beebtrk.ssd -opt 2 -v
+..\..\Bin\beebasm.exe -i beebtrk2.6502 -do beebtrk2.ssd -opt 2 -v
+
 
 
