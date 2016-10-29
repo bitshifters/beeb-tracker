10581e5b1427d2e579019c2df4b3395a1b474d6b
kieranhj <kieran@kconnell.co.uk>
2016-10-29 16:15:24 +0100

    Sonic the Hedgehog disc
    Created Beeb Tracker disc #3 containing 28 tracks from Sonic the
    Hedgehog and Sonic Chaos
    all taken from smspower.org and converted in the usual manner.  Plenty
    of space available on
    the disc.  Could probably do with a better splash image, just done
    quickly in Image2BBC.

--- data/pack.bat
+++ data/pack.bat
@@ -1,3 +1,4 @@
 exomizer.exe raw -c -m 1024 loader_volume1.bin -o loader_volume1.bin.exo
 exomizer.exe raw -c -m 1024 loader_volume2.bin -o loader_volume2.bin.exo
 exomizer.exe raw -c -m 1024 ninja_gaiden.bin -o ninja_gaiden.bin.exo
+exomizer.exe raw -c -m 1024 sonic.bin -o sonic.bin.exo
--- make.bat
+++ make.bat
@@ -2,5 +2,5 @@
 ..\..\Bin\beebasm.exe -i beebtrk.6502 -do beebtrk.ssd -opt 2 -v
 pause
 ..\..\Bin\beebasm.exe -i beebtrk2.6502 -do beebtrk2.ssd -opt 2 -v
-
-
+pause
+..\..\Bin\beebasm.exe -i beebtrk3.6502 -do beebtrk3.ssd -opt 2 -v
